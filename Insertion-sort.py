c5346f0a161fb03450f2c2d89ab7c1dab566f824
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-03 18:44:38 +0200

    Implemented insertion sort

--- Insertion-sort.py
+++ Insertion-sort.py
@@ -1,3 +1,28 @@
 import numpy as np
 
-def insertion_sort()
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i] # Lagrer verdien midlertidig
+
+        j = i - 1 # Vil sjekke alle verdier til venstre for i
+        while (j >= 0 and array[j] > key): 
+            array[j+1] = array[j] # Flytter verdien ved j ett hakk til høyre
+            j -= 1 
+
+        array[j+1] = key # Setter tilbake den midlertidige verdien
+
+
+
+def main():
+    arr = np.array([7, 2, 3, 9, 0, 2, 5])
+
+    insertion_sort(arr)
+
+    print(arr)
+
+main()
+
+"""
+Insertion sort er bra for små lister, men dårligere for lengre lister.
+O(n²)
+"""
